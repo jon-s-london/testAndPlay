f8aced41a46c51e7fd70eba415adfb2278fd3b02
Jon S <118857306+jon-s-london@users.noreply.github.com>
2022-12-14 20:03:11 +0000

    Added the YouTube video link in comments

--- listCompare.py
+++ listCompare.py
@@ -1,5 +1,5 @@
 # 2 lists, one is missing an element from the other. Identify the element. 
-
+# Problem based on https://youtu.be/cdCeU8DJvPM
 
 def find_missing(list1, list2): # Where list2 is always -1 element of list1
     list3 = [x for x in list1 if x not in list2]
