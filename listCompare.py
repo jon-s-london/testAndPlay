a1e0ae20d6820fb99e0390d5dc603506879e8ed5
Jon S <118857306+jon-s-london@users.noreply.github.com>
2022-12-14 20:01:13 +0000

    Initial commit, a play with the coding challenge used in a YouTube video - the video is in the code comments

---
+++ listCompare.py
@@ -0,0 +1,12 @@
+# 2 lists, one is missing an element from the other. Identify the element. 
+
+
+def find_missing(list1, list2): # Where list2 is always -1 element of list1
+    list3 = [x for x in list1 if x not in list2]
+    return list3[0]
+    
+
+l1 = [1]
+l2 = []
+missing = find_missing(l1, l2)
+print(missing)
